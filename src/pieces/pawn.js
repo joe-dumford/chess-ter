053e0234a70c5f3c82c748ddd861c643a1600982
Joe Dumford <dumford.joe@gmail.com>
2019-07-22 16:58:51 -0400

    Fixed bug in pawnJS file

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -14,7 +14,7 @@ export default class Pawn extends Piece {
     isMovePossible(src, dest, isDestEnemyOccupied) {
 
         if (this.player === 1) {
-            if ((dest === src - 8 && !isDestEnemyOccupied) || (des === src - 16 && this.initialPositions[1].indexOf(src) !== -1)) {
+            if ((dest === src - 8 && !isDestEnemyOccupied) || (dest === src - 16 && this.initialPositions[1].indexOf(src) !== -1)) {
                 return true;
             } else if (isDestEnemyOccupied && (dest === src - 9 || dest === src - 7)) {
                 return true;
