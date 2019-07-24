fd348f0571186cb405673a7dbfe743e03ebadb6a
Joe Dumford <dumford.joe@gmail.com>
2019-07-24 15:32:59 -0400

    Updates

---
+++ precache-manifest.d7ad8fbc1baeaead304e5744b7ee5333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd1fca16018d99099410ef2111fcfea",
+    "url": "/chess-ter/index.html"
+  },
+  {
+    "revision": "4819a1a6f56a05f45ce9",
+    "url": "/chess-ter/static/css/main.2a51d35b.chunk.css"
+  },
+  {
+    "revision": "b191b46356834f3bb344",
+    "url": "/chess-ter/static/js/2.a8b47847.chunk.js"
+  },
+  {
+    "revision": "4819a1a6f56a05f45ce9",
+    "url": "/chess-ter/static/js/main.ae87e9ef.chunk.js"
+  },
+  {
+    "revision": "ef94dadcadbe14ffa646",
+    "url": "/chess-ter/static/js/runtime~main.6ac25b9a.js"
+  },
+  {
+    "revision": "bd654982d95d670933d1868380fa7cbe",
+    "url": "/chess-ter/static/media/chess-pic.bd654982.jpg"
+  }
+]);
