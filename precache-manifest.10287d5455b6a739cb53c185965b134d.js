91d917120ca561c31934720de2f2f8e8306fda91
Joe Dumford <dumford.joe@gmail.com>
2020-09-26 14:39:49 -0400

    Updates

---
+++ precache-manifest.10287d5455b6a739cb53c185965b134d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d957636e5639170618784822e8a2b6",
+    "url": "/chess-ter/index.html"
+  },
+  {
+    "revision": "c4930212cbf09ca468cd",
+    "url": "/chess-ter/static/css/main.1228f535.chunk.css"
+  },
+  {
+    "revision": "8a217ef8e484bc6b2665",
+    "url": "/chess-ter/static/js/2.838b3c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess-ter/static/js/2.838b3c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4930212cbf09ca468cd",
+    "url": "/chess-ter/static/js/main.5cead4b4.chunk.js"
+  },
+  {
+    "revision": "646348cc34daf3270f44",
+    "url": "/chess-ter/static/js/runtime-main.d0accaa4.js"
+  },
+  {
+    "revision": "bd654982d95d670933d1868380fa7cbe",
+    "url": "/chess-ter/static/media/chess-pic.bd654982.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chess-ter/precache-manifest.d7ad8fbc1baeaead304e5744b7ee5333.js"
+  "/chess-ter/precache-manifest.10287d5455b6a739cb53c185965b134d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chess-ter/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
